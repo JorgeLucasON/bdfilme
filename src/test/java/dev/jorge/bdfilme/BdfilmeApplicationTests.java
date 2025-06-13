6d00b8309552eb6b7ff69604fa6e9bb70683028c
JorgeLucasON <jorgelucasolivx@gmail.com>
2025-06-13 10:22:55 -0300

    first commit

---
+++ src/test/java/dev/jorge/bdfilme/BdfilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jorge.bdfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdfilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
